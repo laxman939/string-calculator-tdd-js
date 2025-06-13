793cb51355eda69ad27dd0f38249325ab2521615
laxman <ramlaxman947@gmail.com>
2025-06-13 22:46:17 +0530

    RED: Add test for negative number exception

--- stringCalculator.test.js
+++ stringCalculator.test.js
@@ -44,4 +44,17 @@ describe("StringCalculator", () => {
     expect(calculator.add("//|\n1|2|3")).toBe(6);
     expect(calculator.add("//:\n1:2:3:4")).toBe(10);
   });
+
+  // 7. Negative numbers
+  test("should throw an error for negative numbers", () => {
+    expect(() => calculator.add("-2")).toThrow(
+      "Negative numbers not allowed: -2"
+    );
+    expect(() => calculator.add("-1,2")).toThrow(
+      "Negative numbers not allowed: -1,2"
+    );
+    expect(() => calculator.add("1,-2,3")).toThrow(
+      "Negative numbers not allowed: 1,-2,3"
+    );
+  });
 });
