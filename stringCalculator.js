676d3675c57250b64304068e6e759bcea06235ca
laxman <ramlaxman947@gmail.com>
2025-06-13 14:13:10 +0530

    GREEN: Implement new line delimiter support

--- stringCalculator.js
+++ stringCalculator.js
@@ -4,9 +4,10 @@ class StringCalculator {
       return 0;
     }
     // return parseInt(numbers, 10); // Convert string to integer
-    const numberArray = numbers.split(",").map((num) => parseInt(num, 10));
-    console.log(numberArray, "numberArray");
-
+    const normalizedNumbers = numbers.replace(/\n/g, ",");
+    const numberArray = normalizedNumbers
+      .split(",")
+      .map((num) => parseInt(num, 10));
     return numberArray.reduce((sum, num) => sum + num, 0);
   }
 }
